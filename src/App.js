f331e3a698275f935b257971d9055beb6e992290
Rishab Kumar <rishabkumar7@gmail.com>
2020-11-13 14:27:10 -0500

    Added globalstyles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router} from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <Navbar></Navbar>
     </Router>
   );
