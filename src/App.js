b6c0b2c76e02695ccc3d33ab2b2eefdc62da51a8
Rishab Kumar <rishabkumar7@gmail.com>
2020-11-13 16:08:12 -0500

    Added Hero and Sidebar section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router} from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
+      <Hero />
       <Navbar></Navbar>
     </Router>
   );
