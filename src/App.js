730ddc3ea4618ded4ec233f5b43b1af7a6de96b9
Rishab Kumar <rishabkumar7@gmail.com>
2020-11-17 22:22:11 -0500

    Added Fetured section and products

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
 import Hero from './components/Hero';
+import Products from './components/Products';
+import { productData, productDataTwo } from './components/Products/data';
+import Feature from './components/Feature';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
-      <Navbar></Navbar>
+      <Products heading='Choose your favorite' data={productData} />
+      <Feature />
+      <Products heading='Sweet Treats for You' data={productDataTwo} />
+      <Footer />
     </Router>
   );
 }
